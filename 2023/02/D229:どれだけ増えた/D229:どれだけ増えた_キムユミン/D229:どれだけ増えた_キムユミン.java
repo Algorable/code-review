27a127ef2938039d4bee066d788847d059b055d1
devYuMinKim <gimyumin40@gmail.com>
2023-02-14 20:39:26 +0900

    Feat : D229:どれだけ増えた 20230214 キムユミン

---
+++ 2023/02/D229:どれだけ増えた/D229:どれだけ増えた_キムユミン/D229:どれだけ増えた_キムユミン.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        System.out.println(line - 10);
+    }
+}
